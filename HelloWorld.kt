67ab9f5a464e4e43af52b4641d410232e6bf0eff
soudeepghosh <soudeepghosh20@gmail.com>
2023-01-26 15:43:35 +0530

    HelloWorld.kt add in new branch kotlin

---
+++ HelloWorld.kt
@@ -0,0 +1,28 @@
+//import jdk.internal.org.jline.utils.Colors.s
+
+fun main(){
+   /*
+    // print("Hello World")
+    val num1 = 14.0
+    val num2 = 41.0
+    var sum:Double
+    sum = num1+num2 //addition
+    println("The addition of $num1 and $num2 is $sum")
+    var mul = num1*num2 //multiplication
+    println("The multiplication of $num1 and $num2 is $mul")
+    var div = num2/num1 //divition
+    println("The divition of $num1 and $num2 is $div")
+    var mod = num2%num1
+    println("The modulous of $num1 and $num2 is $mod")
+*/
+  //String template
+   //val n:Int = 600;  val str:String = "Cristiano Ronaldo"; println("$str has scored $n league goals!!");
+/*
+    var str1:String = "hello world   "
+    var str2:String = "welcome to world of kotlin!"
+    println("Length of first string ${str1.length}") //length
+    println("in 2nd string character at 12th index is ${str2.get(11)}") // get(index)
+    println("Substring of 2nd string between 20-25 index is ${str2.subSequence(20,25)}") // subSequence(20,25)
+*/
+
+}
